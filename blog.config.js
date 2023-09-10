53c630b6db19f49ca1ef4776355456eb68ab95cd
三鉴（船长） <xiaganyu@163.com>
2023-09-10 11:52:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '老甘个人珍藏', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '老甘个人博客，分享各种有用知识🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址------------------
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.tyhysjc.com', // 网站地址------------------
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，科技博主，AI，AI绘画', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
